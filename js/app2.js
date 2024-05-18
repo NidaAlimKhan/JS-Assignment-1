052a77c7919b250dd9f27f8708ce21faec456ec9
NidaAlimKhan <nidaalimkhan@gmail.com>
2024-05-18 19:46:13 +0500

    change two

---
+++ js/app2.js
@@ -0,0 +1,18 @@
+
+
+let username;
+let myName = 'Nida Alim Khan';
+alert('Hello World');
+alert(myName);
+alert('40 years old');
+let certification = 'Certified Mobile Application Development';
+alert(certification);
+alert('PIZZA\nPIZZ\nPIZ\nPI\nP');
+let email = 'nidaalimkhan@gmail.com';
+alert('My email Address is ' + email);
+let book = 'A smarter way to learn JavaScript';
+alert('I am trying to learn from the book' + book);
+document.write('Yah! I can write HTML content through JavaScript');
+let separate = '▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬'
+alert(separate);
+
